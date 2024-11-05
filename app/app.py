127e716f1e987a8c68a7696a6724554bac882b01
shatada72 <80713915+shatada72@users.noreply.github.com>
2024-11-05 06:09:02 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -14,7 +14,7 @@ def elapsed():
 
 @app.route('/')
 def root():
-    return "Olá TecHubbbers - Super app em (Python)!!!!! (up %s)\n" % elapsed()
+    return "Olá Granado - Super app em (Python)!!!!! (up %s)\n" % elapsed()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
